67744638c0fc0c93d2fca844684ece6b3a3c88b5
adithi-even <adithigopal1409@gmail.com>
2025-03-13 18:21:58 +0530

    association

--- server/models/associations.js
+++ server/models/associations.js
@@ -10,15 +10,3 @@ User.hasMany(Assessment, {foreignKey: 'createdBy'});
 Assessment.belongsTo(User, {foreignKey: 'createdBy'});
 
 Assessment.belongsToMany(Question, {through: AssessmentQuestion, foreignKey: 'assessmentId'});
-Question.belongsToMany(Assessment, {through: AssessmentQuestion, foreignKey: 'questionId'});
-
-Question.hasMany(Option, {foreignKey: 'questionId', onDelete: 'CASCADE'});
-Option.belongsTo(Question, {foreignKey: 'questionId'});
-
-User.hasMany(Result, {foreignKey: 'userId'});
-Assessment.hasMany(Result, {foreignKey: 'assessmentId'});
-
-Result.hasMany(ResultAnswer, {foreignKey: 'resultId', onDelete: 'CASCADE'});
-ResultAnswer.belongsTo(Result, {foreignKey: 'resultId'});
-
-export { User, Assessment, Question, Option, Result, ResultAnswer, AssessmentQuestion };
