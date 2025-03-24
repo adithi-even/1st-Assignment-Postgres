b7a372ed65c9e65749db7fd2c8a2146e7a62f6f9
adithi-even <adithigopal1409@gmail.com>
2025-03-24 15:26:23 +0530

    dashboard is completed now yet to do the test component

--- client/src/Components/dashboard.jsx
+++ client/src/Components/dashboard.jsx
@@ -1,17 +1,49 @@
 import React from 'react';
 import { getAssessments } from '../services/assessmentService';
+import { useState, useEffect } from 'react';
+import {useNavigate} from 'react-router-dom';
 
-function dashboard() {
+
+function Dashboard() {
+
+    const [assessments, setAssessments] = useState([]);
+    const navigate = useNavigate();
+    
      useEffect (()=> {
         const fetchAssessments = async () => {
-           await getAssessments();
-           
-
+            try {
+              const assessmentData = await getAssessments();
+              setAssessments(assessmentData);
+            } catch (error) {
+                console.log(error,"error in the useEffect of the DASHBOARD component");
+                
+            }
         }
-     },[]);
-  return (
-    <div>dashboard</div>
-  )
-}
 
-export default dashboard
+        fetchAssessments();
+     },[]);
+
+     const startTest =(assessmentId) => {
+        navigate(`/assessmet/${assessmentId}`);
+     }
+
+  return (
+    <div>
+        <h2>Available Assessments</h2>
+        <div >
+            {assessments && Array.isArray(assessments) ? (assessments.map((assessment)=>{
+                return (
+                    <div key={assessments.id}>
+                        <h2>{assessment.title}</h2>
+                        <button onClick={()=>startTest(assessment.id)}>Start Test {assessment.id}</button>
+                    </div>
+                )
+            })):(
+                <h3>No assessments found</h3>
+            )}
+        </div>
+    </div>
+  );
+};
+
+export default Dashboard;
--- client/src/services/assessmentService.js
+++ client/src/services/assessmentService.js
@@ -5,12 +5,11 @@ const API_URL = "http://localhost:5000/api/assessments/";
 export const getAssessments = async() => {
     const response = await axios.get(API_URL);
     return response.data;
-
 }
 
 export const getAssessmentById = async(id) => {
     const response = await axios.get(`${API_URL}/:${id}`);
-    return response.data
+    return response.data;
 }
 
 export const createAssessment = async(data) => {
