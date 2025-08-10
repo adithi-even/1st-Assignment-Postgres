7bb403d5662e475a2b92f5978509f42e6e797c49
adithi-even <adithigopal1409@gmail.com>
2025-08-10 19:58:31 +0530

    project setup done

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,16 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
   server: {
+    port: 5174,
+    host: '127.0.0.1',
     proxy: {
       "/api": {
-        target: "http://localhost:5000", // ✅ Make sure this matches your backend URL
+        target: "http://localhost:5000",
         changeOrigin: true,
         secure: false,
       },
